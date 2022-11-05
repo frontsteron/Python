e39774f6e6688d54272f990e89515403b7ec248c
Denis Yershov <anubismclovin@gmail.com>
2022-11-05 14:09:26 +0500

    Task 3 completed

---
+++ Projects/Project_2/Task3.py
@@ -0,0 +1,11 @@
+﻿# Даны две строки. Посчитайте сколько раз
+# каждый символ первой строки встречается во второй
+# «one» «onetwonine» - o – 2, n – 3, e – 2
+
+print('Введи первую строку - ')
+line1 = input()
+print('Введи вторую строку - ')
+line2 = input()
+
+for symbol in set(line1):
+    print(symbol,': ',line2.count(symbol))
