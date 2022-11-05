5400951c97a25880431aba5336dd691de4b30ad7
Denis Yershov <anubismclovin@gmail.com>
2022-11-05 13:42:04 +0500

    Task 1 completed

---
+++ Projects/Project_1/Task1.py
@@ -0,0 +1,12 @@
+﻿# Напишите программу, которая принимает на вход цифру, 
+# обозначающую день недели, и выводит название этого дня недели.
+
+NumberDay_week = int(input('Введите цифру, обозначающую день недели - '))
+if   NumberDay_week == 1: print("Понедельник") #если
+elif NumberDay_week == 2: print("Вторник")  #а если
+elif NumberDay_week == 3: print("Среда")
+elif NumberDay_week == 4: print("Четверг")
+elif NumberDay_week == 5: print("Пятница")
+elif NumberDay_week == 6: print("Суббота")
+elif NumberDay_week == 7: print("Воскресенье")
+else:                     print("Нет такого дня") #иначе
