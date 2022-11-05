33f388d5f2b83b045325529455737ea4f1095ac4
Denis Yershov <anubismclovin@gmail.com>
2022-11-05 14:29:58 +0500

    Task 2 completed

---
+++ Projects/Project_3/Task2.py
@@ -0,0 +1,21 @@
+﻿# В файле находятся названия фруктов.
+# Выведите все фрукты, названия которых начинаются на
+# заданную букву.
+
+def getFirstLetter(): return input()[0]
+print('Введи первую букву фрукта и я тебе покажу фрукты на эту букву - ')
+
+def findFruits(letter:str):
+    with open('fruits.txt', 'r', encoding='utf-8') as inf:
+        fruits = [inf.readline().strip().replace('\n','') for _ in inf]
+    print(fruits)
+    for fruit in fruits:
+        if fruit and fruit[0].lower() == letter.lower():
+            print(fruit)
+
+def main():
+    char = getFirstLetter()
+    findFruits(char)
+
+if __name__ == '__main__':
+    main()
