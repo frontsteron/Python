2bd1b99610c06df6cebba409790d2e74afad6fea
Denis Yershov <anubismclovin@gmail.com>
2022-11-05 14:09:00 +0500

    Task 2 completed

---
+++ Projects/Project_2/Task2.py
@@ -0,0 +1,8 @@
+﻿﻿# Задача 2. Выведите таблицу истинности для 
+# выражения ¬(X ∧ Y) ∨ Z.
+
+print('x', 'y', 'z')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(x, y, z, bool(not (x and y) or z))
