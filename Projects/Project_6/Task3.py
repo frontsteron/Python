d285a81d2369ea173cd292841ea0f1db18ca8642
Denis Yershov <anubismclovin@gmail.com>
2022-11-09 14:12:44 +0500

    Task 3 completed

--- Projects/Project_6/Task3.py
+++ Projects/Project_6/Task3.py
@@ -6,6 +6,6 @@ denominator = 11
 used = set()
 for i in range(1, denominator):
     for j in range(1, denominator + 1):
-        if i < j and i//j != i/j and (n := Fraction(i,j)) not in used:
+        if i < j and i//j != i/j and (n := Fraction(i, j)) not in used:
             print(n)
             used.add(n)
