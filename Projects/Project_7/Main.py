72e1c4a2c67ef88eb7f139b733f4d9020b46f430
Denis Yershov <anubismclovin@gmail.com>
2022-11-12 18:44:18 +0500

    Main file

---
+++ Projects/Project_7/Main.py
@@ -0,0 +1,13 @@
+﻿from os.path import exists
+from CSV_creating import csv_creating
+from Writing import writing_scv
+from Writing import writing_txt
+
+
+path = 'Phonebook.csv'
+valid = exists(path)
+if not valid:
+    csv_creating()
+
+writing_scv()
+writing_txt()
