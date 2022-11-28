e58a17e65b7a5bbc5de81edacd73fe5bc2976405
Denis Yershov <frontsteron@gmail.com>
2022-11-28 11:10:40 +0500

    Task 1 update

--- Projects/Project_10/tech_support_bot.py
+++ Projects/Project_10/tech_support_bot.py
@@ -1,24 +1,19 @@
 ﻿# Напишите бота для техподдержки. Бот должен записывать обращения пользователей в файл.
+# Добавьте боту модуль, который позволяет считывать из файла вопрос, отвечать на него и отправлять ответ обратно пользователю.
 
 import telebot
 
 bot = telebot.TeleBot('TOKEN_BOT')
 
-@bot.message_handler(commands=['start', 'help', 'Привет', 'Здравствуйте'])
+@bot.message_handler(content_types=['text'])
 def send_welcome(message):
-	print(message) #вывод всю информацию о пользователе в консоль
-	bot.reply_to(message, "Здравствуйте! Вас приветствует Бот техподдержки 'Банка №1', если хотите сообщить о потере карты, то напишите /ЯПотерялКарту, если хотите заблокировать крату, то напишите /ЗаблокироватьКарту и следующим сообщением напишите её номер")
-
-@bot.message_handler(commands=['ЯПотерялКарту'])
-def send_answer1(message):
-	bot.reply_to(message, "Заблокируйте карту, для этого напишите /ЗаблокироватьКарту")
-
-@bot.message_handler(commands=['ЗаблокироватьКарту'])
-def send_answer2(message):
-	bot.reply_to(message, "Напишите номер карты и в скором времени карта будет заблокирована")
-
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
+	if 'Привет' in message.text:
+		print(message) #вывод всю информацию о пользователе в консоль
+		bot.reply_to(message, "Здравствуйте, " + message.from_user.first_name + ". Вас приветствует Бот техподдержки 'Банка №1', если хотите сообщить о потере карты, то напишите Я потерял карту, если хотите заблокировать крату, то напишите Заблокировать карту и следующим сообщением напишите её номер")
+	if 'Я потерял карту' in message.text:
+		bot.reply_to(message, "Заблокируйте карту, для этого напишите Заблокировать карту")
+	if 'Заблокировать карту' in message.text:
+		bot.reply_to(message, "Напишите номер карты и в скором времени карта будет заблокирована")
 	data = open('expense.txt', 'a+', encoding='utf-8')
 	data.writelines(message.text + '\n')
 	data.close
